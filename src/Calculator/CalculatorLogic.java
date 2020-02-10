44b0d4182fd2c3dab92eb3d9ea2a671ac3e09d94
Usha-29 <ravuri.saiusha@gmail.com>
2020-02-10 19:10:01 +0530

    committed

---
+++ src/Calculator/CalculatorLogic.java
@@ -0,0 +1,25 @@
+package Calculator;
+
+public class CalculatorLogic {
+	  double calculate(int op1, int op2, String opr) {
+	    	double ans=0;
+	    	switch (opr) {
+	    	case "+": 
+	    		ans=(double)(op1+op2); 
+	    		break;
+	    	case "-":  
+	    		ans=(double)(op1-op2); 
+	    		break;
+	    	case "*":  
+	    		ans=(double)(op1*op2); 
+	    		break;
+	    	case "/": 
+	    		ans=(double)(op1/op2); 
+	    		break;
+	    	 default: 
+	    		 
+	    	}
+	    	
+	     return ans;
+	    }
+}
